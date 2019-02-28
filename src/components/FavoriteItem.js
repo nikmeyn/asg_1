e7b2d248a59f1b9ebb0d0cb64578255f69099483
gio <gio@macmoro.com>
2019-02-27 22:24:45 -0700

    changed center

--- src/components/FavoriteItem.js
+++ src/components/FavoriteItem.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-class FavoriteItem extends React.Component{
+class FavoriteItem extends React.Component {
     render() {
-        const imgURL =  `https://storage.googleapis.com/funwebdev-3rd-travel/square-medium/${this.props.favPhoto.path}`;
+        const imgURL = `https://storage.googleapis.com/funwebdev-3rd-travel/square-medium/${this.props.favPhoto.path}`;
         return (
-            <div className="photoFave" onClick = {this.handleViewClick}>
+            <div className="photoFave wrapper" onClick={this.handleViewClick}>
                 <figure>
-                    <img src={imgURL} className="photoThumb" title={this.props.favPhoto.title} alt={this.props.favPhoto.title} />
+                    <img src={imgURL} className="photoThumb" title={this.props.favPhoto.title}
+                         alt={this.props.favPhoto.title}/>
+                    <span onClick={this.props.onClick} className="close"></span>
                 </figure>
             </div>
         );
     }
+
 }
+
 export default FavoriteItem
