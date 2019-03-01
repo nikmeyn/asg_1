acbc1259a29b3aed0674be8ac84d088210f1eabc
Nik Meyn <meyn.nik@gmail.com>
2019-02-28 18:03:46 -0700

    JSZip API How to / examples Performances / limitations JSZip on Github How to ... Use JSZip Read a file Write a file Examples Read local file Read remote file Give the user its zip file Mini app : downloader Mini app : Downloader This mini application let you choose the files you want in a list, download them, zip them and give the result to the user.
    This demo requires a recent browser, see the howto.
    
    This demo depends on the following libraries:
    
    jQuery (to manipulate the DOM)
    JSZipUtils (to download files)
    bootstrap (for the progress bar)
    result
    js code
    js helpers
    html code
    var Promise = window.Promise;
    if (!Promise) {
        Promise = JSZip.external.Promise;
    }
    
    /**
     * Fetch the content and return the associated promise.
     * @param {String} url the url of the content to fetch.
     * @return {Promise} the promise containing the data.
     */
    function urlToPromise(url) {
        return new Promise(function(resolve, reject) {
            JSZipUtils.getBinaryContent(url, function (err, data) {
                if(err) {
                    reject(err);
                } else {
                    resolve(data);
                }
            });
        });
    }
    
    var $form = $("#download_form").on("submit", function () {
    
        resetMessage();
    
        var zip = new JSZip();
    
        // find every checked item
        $(this).find(":checked").each(function () {
            var $this = $(this);
            var url = $this.data("url");
            var filename = url.replace(/.*\//g, "");
            zip.file(filename, urlToPromise(url), {binary:true});
        });
    
        // when everything has been downloaded, we can trigger the dl
        zip.generateAsync({type:"blob"}, function updateCallback(metadata) {
            var msg = "progression : " + metadata.percent.toFixed(2) + " %";
            if(metadata.currentFile) {
                msg += ", current file = " + metadata.currentFile;
            }
            showMessage(msg);
            updatePercent(metadata.percent|0);
        })
        .then(function callback(blob) {
    
            // see FileSaver.js
            saveAs(blob, "example.zip");
    
            showMessage("done !");
        }, function (e) {
            showError(e);
        });
    
        return false;
    });

--- src/components/ZipFavs.js
+++ src/components/ZipFavs.js
@@ -1,49 +1,58 @@
-<<<<<<< HEAD
+
 import React from "react";
 import JSZipUtils from "jszip-utils";
 import JSZip from "jszip";
 
 
-export default function zipTargetFiles(favs) {
-	
-    if (favs.length <= 1) {
-        return favs;
-    } else {
-		var zip = new JSZip();
-		
-		favs.foreach( photo => {
-			JSZipUtils.getBinaryContent(`https://storage.googleapis.com/funwebdev-3rd-travel/large/${photo.path}`, function (err, data) {
-				if(err) {
-					throw err; // or handle the error
-				}
- 				var zip = new JSZip();
-   				zip.file("picture.png", data, {binary:true});			}
-		)
-		}
-	);
-	}
+var Promise = window.Promise;
+if (!Promise) {
+    Promise = JSZip.external.Promise;
 }
-=======
-// import React from "react";
-// import JSZipUtils from "jszip-utils";
-// import JSZip from "jszip";
-//
-//
-// export default function zipTargetFiles(favs) {
-//
-//     if (favs.length <= 1) {
-//         return favs;
-//     } else {
-// 		var zip = new JSZip();
-//
-// 		favs.foreach( JSZipUtils.getBinaryContent(	"", function (err, data) {
-// 				if(err) {
-// 					throw err; // or handle the error
-// 				}
-//  				var zip = new JSZip();
-//    				zip.file("picture.png", data, {binary:true});			}
-// 		)
-// 	);
-// 	}
-// }
->>>>>>> 0b62a081dc204bc348c34d436a8d525277b19332
+
+function urlToPromise(url) {
+    return new Promise(function(resolve, reject) {
+        JSZipUtils.getBinaryContent(url, function (err, data) {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+var $form = $("#download_form").on("submit", function () {
+
+    resetMessage();
+
+    var zip = new JSZip();
+
+    // find every checked item
+    $(this).find(":checked").each(function () {
+        var $this = $(this);
+        var url = $this.data("url");
+        var filename = url.replace(/.*\//g, "");
+        zip.file(filename, urlToPromise(url), {binary:true});
+    });
+
+    // when everything has been downloaded, we can trigger the dl
+    zip.generateAsync({type:"blob"}, function updateCallback(metadata) {
+        var msg = "progression : " + metadata.percent.toFixed(2) + " %";
+        if(metadata.currentFile) {
+            msg += ", current file = " + metadata.currentFile;
+        }
+        showMessage(msg);
+        updatePercent(metadata.percent|0);
+    })
+    .then(function callback(blob) {
+
+        // see FileSaver.js
+        saveAs(blob, "example.zip");
+
+        showMessage("done !");
+    }, function (e) {
+        showError(e);
+    });
+
+    return false;
+});
