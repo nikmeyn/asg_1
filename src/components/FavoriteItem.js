672deb880854f6041cf4d9b1dcbb14ae60cfb803
gio <gio@macmoro.com>
2019-03-01 17:30:56 -0700

    Added overlay to remove the image.

--- src/components/FavoriteItem.js
+++ src/components/FavoriteItem.js
@@ -1,14 +1,17 @@
 import React from "react";
-
+import '../styles/favoriteItem.css';
 class FavoriteItem extends React.Component {
     render() {
         const imgURL = `https://storage.googleapis.com/funwebdev-3rd-travel/square-medium/${this.props.favPhoto.path}`;
         return (
             <div className="photoFave wrapper" onClick={this.handleViewClick}>
                 <figure>
-                    <img src={imgURL} className="photoThumb" title={this.props.favPhoto.title}
+                    <img className="image" src={imgURL} className="photoThumb" title={this.props.favPhoto.title}
                          alt={this.props.favPhoto.title}/>
-                    <button className="photoThumb" onClick = {this.handleRemoveFavorite }>Remove</button>
+                    <div className="overlay">
+                        <div className="text">Remove</div>
+                    </div>
+                    {/*<button className="photoThumb" onClick = {this.handleRemoveFavorite }>Remove</button>*/}
                     {/*<span onClick={this.handleRemoveFavorite} className="close"></span>*/}
                 </figure>
             </div>
