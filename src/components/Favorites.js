a5613a7557bd37ab47016da85a0edf53cede40d1
gio <gio@macmoro.com>
2019-03-01 19:04:42 -0700

    Toggle added tested working.

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,25 +1,43 @@
 import React from "react";
 import FavoriteItem from './FavoriteItem.js';
 import ZipFavs from './ZipFavs.js';
+import {Collapse, Button} from 'reactstrap';
 
 
 class Favorites extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {collapse: true};
+    }
+
+
     render() {
         if (this.props.favList.length > 0) {
             return (
+                <div>
+                    <Button onClick={this.toggle} style={{marginBottom: '1rem'}}>Show Favourites</Button>
+                    <Collapse isOpen={this.state.collapse}>
+                        <article className="favorites">
 
-                <article className="favorites">
-                    <div className="toggle">
-                        { this.props.favList.map( (p) => <FavoriteItem favPhoto={p} key={p.id} removeFromFavs= {this.props.removeFromFavs}/> )}
-                        <ZipFavs favList = {this.props.favList}/>
 
-                    </div>
+                            {this.props.favList.map((p) => <FavoriteItem favPhoto={p} key={p.id}
+                                                                         removeFromFavs={this.props.removeFromFavs}/>)}
+                            <ZipFavs favList={this.props.favList}/>
 
-                </article>
+
+                        </article>
+                    </Collapse>
+                </div>
             );
-        }else{
-        return null
+        } else {
+            return null
         }
     }
+
+    toggle() {
+        this.setState({collapse: !this.state.collapse});
+    }
 }
+
 export default Favorites
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './styles/index.css';
 import App from './App.js';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
